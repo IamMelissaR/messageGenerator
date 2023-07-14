020788ff9eb75bea9c450918f3888101efaa49dc
IamMelissaR <melissa.a.reiss@gmail.com>
2023-07-14 17:56:56 -0500

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+const randomNum = Math.floor(Math.random() * 4)
+
+    switch (randomNum) {
+        case 1:
+            console.log('You are cool');
+            break;
+        case 2:
+            console.log('You are mediocre');
+            break;
+        case 3:
+            console.log('You are uncool');
+            break;
+    }
+
