6ce75d4104ea613f3c3c52d4c2bf99a009c34a1c
IamMelissaR <melissa.a.reiss@gmail.com>
2023-07-16 12:54:50 -0500

    added words to each case

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const randomNum = Math.floor(Math.random() * 4)
 
     switch (randomNum) {
         case 1:
-            console.log('You are cool');
+            console.log('You are super cool');
             break;
         case 2:
-            console.log('You are mediocre');
+            console.log('You are just mediocre');
             break;
         case 3:
-            console.log('You are uncool');
+            console.log('You are very uncool');
             break;
     }
 
