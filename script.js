985ce60228e5cc5cba1bc583d975c68e1ab353b2
IamMelissaR <melissa.a.reiss@gmail.com>
2023-07-31 18:42:24 -0500

    changed message generator entirely-harry potter themed now

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-const randomNum = Math.floor(Math.random() * 4)
+//function to select random value from each array in the object
+const generateRandomNum = (num) => { Math.floor(Math.random() * num)}
 
-    switch (randomNum) {
-        case 1:
-            console.log('You are super cool');
-            break;
-        case 2:
-            console.log('You are just mediocre');
-            break;
-        case 3:
-            console.log('You are very uncool');
-            break;
+//object storing the arrays to be selected from
+const collectiveHarryPotterInfo = {
+    house: ['Gryffindor', 'Slytherin', 'Ravenvclaw', 'Hufflepuff'],
+    qualityOfYear: ['super easy', 'challenging but satisfying', 'more magical than imaginable', 'dangerous...beware!', 'beyond confusing'],
+    opponent: ['a troll', 'your relection', 'a ghost', 'Voldemort', 'Dumbledore']
+}
+//array to store (push to) the message
+let personalMessage = []
+
+//iterate over the object and then use switch to push the each selected value to the personalMessage array
+for(let property in collectiveHarryPotterInfo) {
+    let optionIdx = generateRandomNum(collectiveHarryPotterInfo[property].length)
+
+    switch(property) {
+        case 'house':
+            personalMessage.push(`What house should you be in...hmm...better be...${collectiveHarryPotterInfo[property][optionIdx]}!`)
+            break
+        case 'qualityOfYear': personalMessage.push(`Your year at Hogwarts will be ${collectiveHarryPotterInfo[property][optionIdx]}.`)
+            break
+        case 'opponent':
+            personalMessage.push(`You will have an epic battle against ${collectiveHarryPotterInfo[property][optionIdx]}`)
+            break
+        default: 
+        personalMessage.push('Uh oh, something has gone wrong')
     }
+}
 
+console.log(personalMessage)
